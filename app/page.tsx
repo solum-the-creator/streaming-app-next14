e28ac1be2e0cfb3c9bc8da18e5994972784e1b94
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-01-17 11:37:46 +0300

    feat: add authentication through integration with the Clerk tool

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
-    <Button size='lg' variant='outline'>
-      Click Me
-    </Button>
+    <div className='flex flex-col gap-y-4'>
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl='/' />
+    </div>
   );
 }
