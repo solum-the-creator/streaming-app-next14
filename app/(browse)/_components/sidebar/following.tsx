cc0edf380ba53f593d3c2858d9946151384b861d
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-01-30 16:55:50 +0300

    fix: change the output data for the Following and Recommended lists

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -6,7 +6,9 @@ import { Follow, Stream, User } from '@prisma/client';
 import { UserItem, UserItemSkeleton } from './user-item';
 
 interface FollowingProps {
-  data: (Follow & { following: User & { stream: Stream | null } })[];
+  data: (Follow & {
+    following: User & { stream: { isLive: boolean } | null };
+  })[];
 }
 
 export const Following = ({ data }: FollowingProps) => {
--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -42,7 +42,11 @@ export const getRecommended = async () => {
         ],
       },
       include: {
-        stream: true,
+        stream: {
+          select: {
+            isLive: true,
+          },
+        },
       },
       orderBy: {
         createdAt: 'desc',
@@ -54,7 +58,11 @@ export const getRecommended = async () => {
         createdAt: 'desc',
       },
       include: {
-        stream: true,
+        stream: {
+          select: {
+            isLive: true,
+          },
+        },
       },
     });
   }
