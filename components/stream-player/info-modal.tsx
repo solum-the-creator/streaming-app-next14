8aa9b16e35a793c1d2ede77fa65e9e7b0667fd94
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-02-01 17:03:22 +0300

    feat: add the ability for the user to change the thumbnail of the stream

--- components/stream-player/info-modal.tsx
+++ components/stream-player/info-modal.tsx
@@ -14,6 +14,11 @@ import { Label } from '../ui/label';
 import { Input } from '../ui/input';
 import { updateStream } from '@/actions/stream';
 import { toast } from 'sonner';
+import { UploadDropzone } from '@/lib/uploadthing';
+import { useRouter } from 'next/navigation';
+import { Hint } from '../hint';
+import { Trash } from 'lucide-react';
+import Image from 'next/image';
 
 interface InfoModalProps {
   initialName: string;
@@ -24,9 +29,23 @@ export const InfoModal = ({
   initialName,
   initialThumbnailUrl,
 }: InfoModalProps) => {
+  const router = useRouter();
   const closeRef = useRef<ElementRef<'button'>>(null);
   const [isPending, startTransition] = useTransition();
   const [name, setName] = useState(initialName);
+  const [thumbnailUrl, setThumbnailUrl] = useState(initialThumbnailUrl);
+
+  const onRemove = () => {
+    startTransition(() => {
+      updateStream({ thumbnailUrl: null })
+        .then(() => {
+          toast.success('Thumbnail removed');
+          setThumbnailUrl('');
+          closeRef?.current?.click();
+        })
+        .catch(() => toast.error('Something went wrong'));
+    });
+  };
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -66,6 +85,52 @@ export const InfoModal = ({
               disabled={isPending}
             />
           </div>
+
+          <div className='space-y-2'>
+            <Label>Thumbnail</Label>
+            {thumbnailUrl ? (
+              <div className='relative aspect-video rounded-xl overflow-hidden border border-white/10'>
+                <div className='absolute top-2 right-2 z-[10]'>
+                  <Hint asChild side='left' label='Remove thumbnail'>
+                    <Button
+                      type='button'
+                      disabled={isPending}
+                      onClick={onRemove}
+                      className='h-auto w-auto p-1.5'
+                    >
+                      <Trash className='h-4 w-4' />
+                    </Button>
+                  </Hint>
+                </div>
+                <Image
+                  fill
+                  alt='Thumbnail'
+                  src={thumbnailUrl}
+                  className='object-cover'
+                />
+              </div>
+            ) : (
+              <div className='rounded-xl border outline-dashed outline-muted'>
+                <UploadDropzone
+                  endpoint='thumbnailUploader'
+                  appearance={{
+                    label: {
+                      color: '#ffffff',
+                    },
+                    allowedContent: {
+                      color: '#ffffff',
+                    },
+                  }}
+                  onClientUploadComplete={(res) => {
+                    setThumbnailUrl(res?.[0]?.url);
+                    router.refresh();
+                    closeRef?.current?.click();
+                  }}
+                />
+              </div>
+            )}
+          </div>
+
           <div className='flex justify-between'>
             <DialogClose ref={closeRef} asChild>
               <Button type='button' variant={'ghost'}>
