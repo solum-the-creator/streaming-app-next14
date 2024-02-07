aac15bc9edb736dfb8954129e2ea8e39e619b4dd
Andrew Kostukevich <mr.kostukevi4@mail.ru>
2024-02-07 10:38:36 +0300

    fix: add data protection

--- lib/search-service.ts
+++ lib/search-service.ts
@@ -75,7 +75,12 @@ export const getSearch = async (term?: string) => {
           },
         ],
       },
-      include: {
+      select: {
+        id: true,
+        name: true,
+        isLive: true,
+        thumbnailUrl: true,
+        updatedAt: true,
         user: true,
       },
       orderBy: [
